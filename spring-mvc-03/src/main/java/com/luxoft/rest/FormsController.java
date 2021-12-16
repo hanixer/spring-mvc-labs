4e6a5e35dd5ee0434225d9b062dee39ca23c2e67
hanixer <hanixer@mail.ru>
2021-12-16 14:02:13 +0200

    Add ability to create an account

--- spring-mvc-03/src/main/java/com/luxoft/rest/FormsController.java
+++ spring-mvc-03/src/main/java/com/luxoft/rest/FormsController.java
@@ -8,10 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -63,6 +61,18 @@ public class FormsController {
     @PostMapping("/delete")
     public String delete(long id) {
         accountService.delete(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/create")
+    public String create() {
+        return "create";
+    }
+
+    @PostMapping("/create")
+    public String create(String holder, int balance) {
+        Account account = new Account(holder, balance);
+        accountService.create(account);
         return "redirect:/";
     }
 
